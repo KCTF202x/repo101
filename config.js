be73aad55287e084817f8c44686c8cce3dda6e6b
KCTF202x <knightsquad.knight.vii@gmail.com>
2021-12-28 03:36:16 +0600

    first commit

---
+++ config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  app: {
+    px: "PREFIX",
+    token: "YOUR-PERSONAL-TOKEN",
+    playing: "by Saif ❤️",
+  },
+
+  opt: {
+    DJ: {
+      enabled: false,
+      roleName: "DJ",
+      commands: [
+        "back",
+        "clear",
+        "filter",
+        "loop",
+        "pause",
+        "resume",
+        "seek",
+        "shuffle",
+        "skip",
+        "stop",
+        "volume",
+      ],
+    },
+    maxVol: 100,
+    loopMessage: false,
+    discordPlayer: {
+      ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+      },
+    },
+  },
+};
