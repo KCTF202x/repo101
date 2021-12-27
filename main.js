74bf8c5679108c685d4c52dd18c191d605b53977
KCTF202x <knightsquad.knight.vii@gmail.com>
2021-12-28 03:42:56 +0600

    KCTF{version_control_is_awesome}

--- main.js
+++ main.js
@@ -18,4 +18,16 @@ global.player = new Player(client, client.config.opt.discordPlayer);
 require('./src/loader');
 require('./src/events');
 
+bot.on("guildMemberAdd", (member) => {
+  let ctx = db.get(`welchannel_${member.guild.id}`);
+  if (ctx === null) {
+    return; 
+  }
+  const emb1 = new Discord.MessageEmbed()
+    .setColor("RED")
+    .setAuthor(member.user.username, member.user.avatarURL())
+    .setDescription(`Welcome Buddy!!!Have Fun!!`);
+  bot.channel.cache.get(ctx).send(emb1);
+})
+
 client.login(client.config.app.token);
